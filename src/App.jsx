da68e91f36aecdbb616f342622c28483c7015a46
adx <nogo22@naver.com>
2024-06-05 15:27:14 +0900

    refactor: 상품 이미지 미리 로드하는 로직 상단으로 재이동
    - app.jsx -> main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,9 @@ import AdminLandingPage from "./pages/AdminLandingPage/index.jsx";
 import OrderListPage from "./pages/AdminOrderListPage/index.jsx";
 import QROrderPage from "./pages/QROrderPage/index.jsx";
 import MenuEditPage from "./pages/AdminMenuEditPage/index.jsx";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import { useQuery } from "react-query";
 import { getProduct } from "./network/request/supabase.js";
 import { useEffect, useRef } from "react";
-
-const queryClient = new QueryClient();
 
 function App() {
   const { data } = useQuery("products", getProduct);
@@ -34,21 +32,19 @@ function App() {
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/locker" element={<LockerKeyPage />} />
-            <Route path="/order" element={<OrderPage />} />
-            <Route path="/qr-order/:token" element={<QROrderPage />} />
-            <Route path="/admin" element={<AdminLandingPage />} />
-            <Route path="/admin/order" element={<OrderListPage />} />
-            <Route path="/admin/menu" element={<MenuEditPage />} />
-          </Routes>
-        </BrowserRouter>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/locker" element={<LockerKeyPage />} />
+          <Route path="/order" element={<OrderPage />} />
+          <Route path="/qr-order/:token" element={<QROrderPage />} />
+          <Route path="/admin" element={<AdminLandingPage />} />
+          <Route path="/admin/order" element={<OrderListPage />} />
+          <Route path="/admin/menu" element={<MenuEditPage />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
